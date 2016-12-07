4f9db6c2f3a2075fde739d22645ac30692f31da6
Harold Portocarrero <hportocarrero@latamautos.com>
2016-12-07 11:14:09 -0500

    cambio en endpoint para provocar error

--- src/test/scala/com/latamautos/SwaggerValidatorPactConsumerTest.scala
+++ src/test/scala/com/latamautos/SwaggerValidatorPactConsumerTest.scala
@@ -23,7 +23,7 @@ class SwaggerValidatorPactConsumerTest extends FunSpec with Matchers {
             .description("Fetching least secure auth token ever")
             .uponReceiving(
               method = GET,
-              path = "/questions2/test",
+              path = "/questions2/test1",
               query = None,
               headers = Map("Accept" -> "application/json"),
               body = None,
